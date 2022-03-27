b8ce5586f74b83dd07b3f7bc931890847e9cf080
Mohammed Elghamry <44609329+melgmry0101b@users.noreply.github.com>
2022-03-28 00:55:56 +0200

    Initial commit

---
+++ src/LeanCameraCapture/AssemblyInfo.cpp
@@ -0,0 +1,23 @@
+using namespace System;
+using namespace System::Resources;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"LeanCameraCapture")] ;
+[assembly:AssemblyDescriptionAttribute(L"Lean Camera Capture core library")] ;
+[assembly:AssemblyConfigurationAttribute(_APP_VER_CONFIGURATION)] ;
+[assembly:AssemblyCompanyAttribute(_APP_VER_COMPANY)] ;
+[assembly:AssemblyProductAttribute(_APP_VER_PRODUCT)] ;
+[assembly:AssemblyCopyrightAttribute(_APP_VER_COPYRIGHT)] ;
+[assembly:AssemblyCultureAttribute(L"")] ;
+[assembly:NeutralResourcesLanguage(L"en-US", UltimateResourceFallbackLocation::MainAssembly)] ;
+
+[assembly:AssemblyFileVersionAttribute(_APP_VER_FILE_VERSION)] ;
+[assembly:AssemblyInformationalVersionAttribute(_APP_VER_INFORMATIONAL_VERSION)] ;
+[assembly:AssemblyVersionAttribute(_APP_VER_ASSEMBLY_VERSION)] ;
+
+[assembly:InternalsVisibleTo(_APP_TESTS_PROJECT)] ;
+
+[assembly:ComVisible(false)] ;
---
+++ src/LeanCameraCapture/macros.h
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------*\
+ *
+ * macros.h
+ *   LeanCameraCapture
+ *     lean-camera-capture
+ *
+ * MIT - see LICENSE at root directory
+ *
+ * CREATED: 2022-3-27 11:35 PM
+ * AUTHORS: Mohammed Elghamry <elghamry.connect[at]outlook[dot]com>
+ *
+\*-----------------------------------------------------------------*/
+
+#ifndef APSTUDIO_INVOKED
+#pragma once
+#endif
+
+#define STRINGIZE(x) #x
+#define STRINGIZE_EXPRESSION(x) STRINGIZE(x)
