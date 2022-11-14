60d148711b4268a0b36839d75732c0fe336aa396
Mohammed Elghamry <44609329+melgmry0101b@users.noreply.github.com>
2022-11-14 22:54:10 +0200

    feat: primary working demo? well it starts to show something!

--- demos/LeanCameraCapture.Demos.WinForms/MainForm.Designer.cs
+++ demos/LeanCameraCapture.Demos.WinForms/MainForm.Designer.cs
@@ -29,51 +29,112 @@ namespace LeanCameraCapture.Demos.WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.uiBtnLoad = new System.Windows.Forms.Button();
-            this.uiCmbDevices = new System.Windows.Forms.ComboBox();
-            this.uiLblDevicesHeader = new System.Windows.Forms.Label();
+            this.UiBtnLoad = new System.Windows.Forms.Button();
+            this.UiCmbDevices = new System.Windows.Forms.ComboBox();
+            this.UiLblDevicesHeader = new System.Windows.Forms.Label();
+            this.UiPicViewer = new System.Windows.Forms.PictureBox();
+            this.UiGrpViewer = new System.Windows.Forms.GroupBox();
+            this.UiBtnSaveStill = new System.Windows.Forms.Button();
+            this.UiBtnStopCapture = new System.Windows.Forms.Button();
+            this.UiBtnStartCapture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UiPicViewer)).BeginInit();
+            this.UiGrpViewer.SuspendLayout();
             this.SuspendLayout();
             // 
-            // uiBtnLoad
+            // UiBtnLoad
             // 
-            this.uiBtnLoad.Location = new System.Drawing.Point(376, 33);
-            this.uiBtnLoad.Name = "uiBtnLoad";
-            this.uiBtnLoad.Size = new System.Drawing.Size(114, 23);
-            this.uiBtnLoad.TabIndex = 0;
-            this.uiBtnLoad.Text = "Load Devices";
-            this.uiBtnLoad.UseVisualStyleBackColor = true;
-            this.uiBtnLoad.Click += new System.EventHandler(this.uiBtnLoad_Click);
+            this.UiBtnLoad.Location = new System.Drawing.Point(504, 33);
+            this.UiBtnLoad.Name = "UiBtnLoad";
+            this.UiBtnLoad.Size = new System.Drawing.Size(114, 23);
+            this.UiBtnLoad.TabIndex = 0;
+            this.UiBtnLoad.Text = "Load Devices";
+            this.UiBtnLoad.UseVisualStyleBackColor = true;
+            this.UiBtnLoad.Click += new System.EventHandler(this.UiBtnLoad_Click);
             // 
-            // uiCmbDevices
+            // UiCmbDevices
             // 
-            this.uiCmbDevices.FormattingEnabled = true;
-            this.uiCmbDevices.Location = new System.Drawing.Point(12, 33);
-            this.uiCmbDevices.Name = "uiCmbDevices";
-            this.uiCmbDevices.Size = new System.Drawing.Size(358, 23);
-            this.uiCmbDevices.TabIndex = 1;
+            this.UiCmbDevices.FormattingEnabled = true;
+            this.UiCmbDevices.Location = new System.Drawing.Point(12, 33);
+            this.UiCmbDevices.Name = "UiCmbDevices";
+            this.UiCmbDevices.Size = new System.Drawing.Size(486, 23);
+            this.UiCmbDevices.TabIndex = 1;
             // 
-            // uiLblDevicesHeader
+            // UiLblDevicesHeader
             // 
-            this.uiLblDevicesHeader.AutoSize = true;
-            this.uiLblDevicesHeader.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.uiLblDevicesHeader.ForeColor = System.Drawing.Color.SteelBlue;
-            this.uiLblDevicesHeader.Location = new System.Drawing.Point(12, 9);
-            this.uiLblDevicesHeader.Name = "uiLblDevicesHeader";
-            this.uiLblDevicesHeader.Size = new System.Drawing.Size(250, 21);
-            this.uiLblDevicesHeader.TabIndex = 2;
-            this.uiLblDevicesHeader.Text = "Available Camera Capture Devices:";
+            this.UiLblDevicesHeader.AutoSize = true;
+            this.UiLblDevicesHeader.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UiLblDevicesHeader.ForeColor = System.Drawing.Color.SteelBlue;
+            this.UiLblDevicesHeader.Location = new System.Drawing.Point(12, 9);
+            this.UiLblDevicesHeader.Name = "UiLblDevicesHeader";
+            this.UiLblDevicesHeader.Size = new System.Drawing.Size(250, 21);
+            this.UiLblDevicesHeader.TabIndex = 2;
+            this.UiLblDevicesHeader.Text = "Available Camera Capture Devices:";
+            // 
+            // UiPicViewer
+            // 
+            this.UiPicViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UiPicViewer.Location = new System.Drawing.Point(6, 22);
+            this.UiPicViewer.Name = "UiPicViewer";
+            this.UiPicViewer.Size = new System.Drawing.Size(480, 360);
+            this.UiPicViewer.TabIndex = 3;
+            this.UiPicViewer.TabStop = false;
+            // 
+            // UiGrpViewer
+            // 
+            this.UiGrpViewer.Controls.Add(this.UiBtnSaveStill);
+            this.UiGrpViewer.Controls.Add(this.UiBtnStopCapture);
+            this.UiGrpViewer.Controls.Add(this.UiBtnStartCapture);
+            this.UiGrpViewer.Controls.Add(this.UiPicViewer);
+            this.UiGrpViewer.Location = new System.Drawing.Point(12, 62);
+            this.UiGrpViewer.Name = "UiGrpViewer";
+            this.UiGrpViewer.Size = new System.Drawing.Size(614, 395);
+            this.UiGrpViewer.TabIndex = 4;
+            this.UiGrpViewer.TabStop = false;
+            this.UiGrpViewer.Text = "Viewport";
+            // 
+            // UiBtnSaveStill
+            // 
+            this.UiBtnSaveStill.Location = new System.Drawing.Point(492, 80);
+            this.UiBtnSaveStill.Name = "UiBtnSaveStill";
+            this.UiBtnSaveStill.Size = new System.Drawing.Size(114, 23);
+            this.UiBtnSaveStill.TabIndex = 7;
+            this.UiBtnSaveStill.Text = "Save Still";
+            this.UiBtnSaveStill.UseVisualStyleBackColor = true;
+            // 
+            // UiBtnStopCapture
+            // 
+            this.UiBtnStopCapture.Location = new System.Drawing.Point(492, 51);
+            this.UiBtnStopCapture.Name = "UiBtnStopCapture";
+            this.UiBtnStopCapture.Size = new System.Drawing.Size(114, 23);
+            this.UiBtnStopCapture.TabIndex = 6;
+            this.UiBtnStopCapture.Text = "Stop Capture";
+            this.UiBtnStopCapture.UseVisualStyleBackColor = true;
+            this.UiBtnStopCapture.Click += new System.EventHandler(this.UiBtnStopCapture_Click);
+            // 
+            // UiBtnStartCapture
+            // 
+            this.UiBtnStartCapture.Location = new System.Drawing.Point(492, 22);
+            this.UiBtnStartCapture.Name = "UiBtnStartCapture";
+            this.UiBtnStartCapture.Size = new System.Drawing.Size(114, 23);
+            this.UiBtnStartCapture.TabIndex = 5;
+            this.UiBtnStartCapture.Text = "Start Capture";
+            this.UiBtnStartCapture.UseVisualStyleBackColor = true;
+            this.UiBtnStartCapture.Click += new System.EventHandler(this.UiBtnStartCapture_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(502, 450);
-            this.Controls.Add(this.uiLblDevicesHeader);
-            this.Controls.Add(this.uiCmbDevices);
-            this.Controls.Add(this.uiBtnLoad);
+            this.ClientSize = new System.Drawing.Size(634, 467);
+            this.Controls.Add(this.UiGrpViewer);
+            this.Controls.Add(this.UiLblDevicesHeader);
+            this.Controls.Add(this.UiCmbDevices);
+            this.Controls.Add(this.UiBtnLoad);
             this.Name = "MainForm";
             this.Text = "LeanCameraCapture WinForms Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.UiPicViewer)).EndInit();
+            this.UiGrpViewer.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,9 +142,14 @@ namespace LeanCameraCapture.Demos.WinForms
 
         #endregion
 
-        private System.Windows.Forms.Button uiBtnLoad;
-        private System.Windows.Forms.ComboBox uiCmbDevices;
-        private System.Windows.Forms.Label uiLblDevicesHeader;
+        private System.Windows.Forms.Button UiBtnLoad;
+        private System.Windows.Forms.ComboBox UiCmbDevices;
+        private System.Windows.Forms.Label UiLblDevicesHeader;
+        private System.Windows.Forms.PictureBox UiPicViewer;
+        private System.Windows.Forms.GroupBox UiGrpViewer;
+        private System.Windows.Forms.Button UiBtnSaveStill;
+        private System.Windows.Forms.Button UiBtnStopCapture;
+        private System.Windows.Forms.Button UiBtnStartCapture;
     }
 }
 
--- demos/LeanCameraCapture.Demos.WinForms/MainForm.cs
+++ demos/LeanCameraCapture.Demos.WinForms/MainForm.cs
@@ -1,26 +1,40 @@
+/*-----------------------------------------------------------------*\
+ *
+ * MainForm.cs
+ *   LeanCameraCapture.Demos.WinForms
+ *     lean-camera-capture
+ *
+ * MIT - see LICENSE at root directory
+ *
+ * CREATED: 2022-5-14 09:35 AM
+ * AUTHORS: Mohammed Elghamry <elghamry.connect[at]outlook[dot]com>
+ *
+\*-----------------------------------------------------------------*/
+
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace LeanCameraCapture.Demos.WinForms
 {
     public partial class MainForm : Form
     {
+        private CameraCaptureReader? m_cameraReader;
+
+        private Bitmap? m_bitmap;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void uiBtnLoad_Click(object sender, EventArgs e)
+        private void UiBtnLoad_Click(object sender, EventArgs e)
         {
-            uiCmbDevices.DataSource = null;
+            UiCmbDevices.DataSource = null;
 
             ReadOnlyCollection<CameraCaptureDevice> captureDevices;
             try
@@ -37,8 +51,120 @@ namespace LeanCameraCapture.Demos.WinForms
                 return;
             }
 
-            uiCmbDevices.DisplayMember = "DeviceName";
-            uiCmbDevices.DataSource = captureDevices;
+            UiCmbDevices.DisplayMember = "DeviceName";
+            UiCmbDevices.ValueMember = "DeviceSymbolicLink";
+            UiCmbDevices.DataSource = captureDevices;
+        }
+
+        private void UiBtnStartCapture_Click(object sender, EventArgs e)
+        {
+            // If the current reader has the same device as the selected, do nothing,
+            //  else, close the current reader.
+            if (m_cameraReader is not null)
+            {
+                if (UiCmbDevices.SelectedValue as string == m_cameraReader.Device.DeviceSymbolicLink && m_cameraReader.IsOpen)
+                {
+                    return;
+                }
+
+                m_cameraReader.Close();
+                m_cameraReader.Dispose();
+                m_cameraReader = null;
+            }
+
+            if (UiCmbDevices.SelectedItem is null) { return; }
+
+            // Open camera reader
+            try
+            {
+                m_cameraReader = new CameraCaptureReader(UiCmbDevices.SelectedItem as CameraCaptureDevice);
+                m_cameraReader.ReadSampleSucceeded += CameraReader_ReadSampleSucceeded;
+                m_cameraReader.ReadSampleFailed += CameraReader_ReadSampleFailed;
+
+                // Open the reader
+                m_cameraReader.Open();
+
+                // Issue the first read
+                m_cameraReader.ReadSample();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(
+                    "Error occurred during starting capture.\r\n" + ex.ToString(),
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (CameraCaptureException ex)
+            {
+                MessageBox.Show(
+                    $"Error occurred during starting capture. code `0x{ex.HResult:X}`\r\n" + ex.ToString(),
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void UiBtnStopCapture_Click(object sender, EventArgs e)
+        {
+            if (m_cameraReader is not null)
+            {
+                m_cameraReader.Close();
+                m_cameraReader.Dispose();
+                m_cameraReader = null;
+            }
+        }
+
+        private void CameraReader_ReadSampleFailed(object? sender, ReadSampleFailedEventArgs e)
+        {
+            Debug.WriteLine($"We got an error `{e.ErrorString}` with code `0x{e.HResult:X}`");
+
+            //MessageBox.Show(
+            //    $"Error during reading sample. code `0x{e.HResult:X}`\r\n" + e.ErrorString,
+            //    "Error",
+            //    MessageBoxButtons.OK,
+            //    MessageBoxIcon.Error);
+
+            m_cameraReader?.ReadSample();
+        }
+
+        private void CameraReader_ReadSampleSucceeded(object? sender, ReadSampleSucceededEventArgs e)
+        {
+            Debug.WriteLine($"We got a sample. {e.WidthInPixels}x{e.HeightInPixels}@{e.BytesPerPixel}");
+
+            if (m_bitmap is null || m_bitmap.Width != e.WidthInPixels || m_bitmap.Height != e.HeightInPixels)
+            {
+                m_bitmap = new Bitmap((int)e.WidthInPixels, (int)e.HeightInPixels, PixelFormat.Format32bppRgb);
+                UiPicViewer.Image = m_bitmap;
+                Invoke(() => UiPicViewer.Refresh());
+            }
+
+            var bmpData = m_bitmap.LockBits(new Rectangle(0, 0, m_bitmap.Width, m_bitmap.Height), ImageLockMode.WriteOnly, m_bitmap.PixelFormat);
+            Marshal.Copy(e.GetBuffer(), 0, bmpData.Scan0, bmpData.Stride * bmpData.Height);
+            m_bitmap.UnlockBits(bmpData);
+
+            Invoke(() => UiPicViewer.Refresh());
+
+            try
+            {
+                m_cameraReader?.ReadSample();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(
+                    "Error during reading sample.\r\n" + ex.ToString(),
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (CameraCaptureException ex)
+            {
+                MessageBox.Show(
+                    $"Error during reading sample. code `0x{ex.HResult:X}`\r\n" + ex.ToString(),
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
